2b962a2b6b8b8df7646c5358f4b8968e06100b1a
Edson Garcia <ednosmab@yahoo.com.br>
2023-07-24 11:50:25 -0300

    inc método p/ substituir algoritmo de encripitação de senha

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -29,6 +29,18 @@ class LoginController implements Controller
 
         $correctPassword = password_verify($password, $userData['password'] ?? '');
 
+        if(password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)){
+            $stmt = $this->pdo->query('UPDATE users SET password = ? WHERE id = ?');
+
+            /**
+             * Quando houver a necessidade atualizar o algoritimo de encriptação de senha
+             * substituir a constante PASSWORD_ARGON2ID 
+             */
+            $stmt->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
+            $stmt->bindValue(2, $userData['id']);
+            $stmt->execute();
+        }
+
         if($correctPassword == false){
             header('Location: /login?success=0');
             return;
