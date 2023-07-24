dbb4d4bde8079bc27de3ffb75f82678a25f5497d
Edson Garcia <ednosmab@yahoo.com.br>
2023-07-24 12:46:29 -0300

    refatorando para contextualizar a variável $fileTempName

--- src/Model/ImageUpload/ImageUploadModel.php
+++ src/Model/ImageUpload/ImageUploadModel.php
@@ -15,14 +15,14 @@ class ImageUploadModel
     
     public function saveImage($video): bool
     {
-        $fileTempName = pathinfo($this->imageName, PATHINFO_BASENAME);
+        $safeFileName = pathinfo($this->imageName, PATHINFO_BASENAME);
 
         if($this->imageError === UPLOAD_ERR_OK){
             $success =  move_uploaded_file(
                 $this->imageTmpName,
-                __DIR__ . '/../../../public/img/uploads/' . $fileTempName
+                __DIR__ . '/../../../public/img/uploads/' . $safeFileName
             );
-            $video->setFilePath($fileTempName);
+            $video->setFilePath($safeFileName);
             return $success;
         }
         return false;
